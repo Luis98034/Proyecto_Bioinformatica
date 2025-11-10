14fddade54cf172a8ee33f59d041d4d95ad31b7e
kerencf <143008733+kerencf@users.noreply.github.com>
2025-11-10 16:12:59 -0600

    datos a considerar

--- Proyecto_Final_Bioinfo/01_Scripts/Raw_Script_02.R
+++ Proyecto_Final_Bioinfo/01_Scripts/Raw_Script_02.R
@@ -1,1 +1,10 @@
 ## Segunda parte para desarrollar los ciclos que estaremos manejando para cada mosca ? 
+
+
+## Qué datos necesitamos considerar para calcular el intervalo postmorten ? 
+
+# Ciclo de vida de las moscas 
+# Temperatura por día 
+# instar en el que se encontró a la mosca 
+ 
+
