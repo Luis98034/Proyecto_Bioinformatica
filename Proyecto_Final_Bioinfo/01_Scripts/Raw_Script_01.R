445faebe0327aa6eef0c10ae0e343f7b620348aa
kerencf <143008733+kerencf@users.noreply.github.com>
2025-11-18 21:37:52 -0600

    En este script deberíamos solo poner la función final

--- Proyecto_Final_Bioinfo/01_Scripts/Raw_Script_01.R
+++ Proyecto_Final_Bioinfo/01_Scripts/Raw_Script_01.R
@@ -1,3 +1,8 @@
-####Este debe de ser el espacio en el que armemos el proyecto
 
-#Prueba de pull
+#### Funcion/es  para determinar el intervalo post morten del espécimen 
+
+
+# Valor de entrada será un archivo cvs con los datos de la temperatura 
+
+
+
